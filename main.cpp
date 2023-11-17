fa460694f92e07d92de408c0c10cf5250a7b05f3
MagicTINTIN <valentinservieres@gmail.com>
2023-11-17 19:07:45 +0200

    add getName + changing dataset structure

--- main.cpp
+++ main.cpp
@@ -15,21 +15,31 @@ const int MAIN_PROCESS = 0;
 const int TAG_PARTIAL_ARRAY = 2;
 const int TAG_PARTIAL_SUM = 1;
 
+std::string getName(std::string const &name, int const &val, int const &max) {
+	int nb0max = std::floor(std::log10(std::max(1,max)));
+	int nb0val = std::floor(std::log10(std::max(1,val)));
+	std::string output = name;
+	for (size_t i = 0; i < (nb0max - nb0val); i++)
+	{
+		output += "0";
+	}
+	output += std::to_string(val) + ".jpg";
+	return output;
+}
 
 long processImg(std::string &imgPath)
 {
 	return combineColors(averageColorImg(imgPath));
 }
 
-long averageMultipleImages(int *imageSet, int const &nbImages, int const &processNb, int &totalSize)
+long averageMultipleImages(int *imageSet, int const &nbImages, int const &processNb, int const &totalSize, std::string const &prefix)
 {
 	std::string printedValue = "Process " + std::to_string(processNb) + " received\n";
 	rgb values(0,0,0);
 	for (size_t i = 0; i < nbImages; i++)
 	{
-		// std::setw(6) << std::setfill('0') << std::right
-		// values += processImg()
-		printedValue += "[" + std::to_string(i) + ":" + std::to_string(imageSet[i]) + "] ";
+		std::string filename = getName(prefix, imageSet[i], totalSize);
+		printedValue += filename + " ";
 	}
 	std::cout << printedValue << std::endl;
 
@@ -57,31 +67,31 @@ int main(int argc, char const *argv[])
 	int totalElements(0);
 
 	std::ifstream f(argv[1]);
-	json jsonArray = json::parse(f);
+	json dataSetJSON = json::parse(f);
+	f.close();
 
 	MPI::Init();
 	int rank = MPI::COMM_WORLD.Get_rank();
 	int totalProcesses = MPI::COMM_WORLD.Get_size();
 
-	totalElements = jsonArray.size();
+	totalElements = dataSetJSON["sequenceNumbers"].size();
 	elementsPerProcess = totalElements / totalProcesses;
 
 	if (rank == 0)
 	{
-		imageSet = new int[jsonArray.size()];
-		for (std::size_t i = 0; i < jsonArray.size(); ++i)
+		imageSet = new int[dataSetJSON["sequenceNumbers"].size()];
+		for (std::size_t i = 0; i < dataSetJSON["sequenceNumbers"].size(); ++i)
 		{
-			imageSet[i] = jsonArray[i];
+			imageSet[i] = dataSetJSON["sequenceNumbers"][i];
 		}
 
 		elementsNotProcessed = totalElements % totalProcesses;
 		std::cout << "Getting average color of image sequence: " << imageSet[0] << "-" << imageSet[elementsPerProcess * totalProcesses - 1] << " (" << elementsNotProcessed << " images ignored)" << std::endl;
 	}
-	f.close();
 
 	int partialImageSet[elementsPerProcess];
 	MPI::COMM_WORLD.Scatter(imageSet, elementsPerProcess, MPI::INT, partialImageSet, elementsPerProcess, MPI::INT, 0);
-	long average = averageMultipleImages(partialImageSet, elementsPerProcess, rank, totalElements);
+	long average = averageMultipleImages(partialImageSet, elementsPerProcess, rank, totalElements, dataSetJSON["sequenceName"].get<std::string>());
 
 	long *averagesPartsImagesColor = nullptr;
 	if (rank == 0)
