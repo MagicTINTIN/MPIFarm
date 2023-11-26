945fc368bfb10818a18360ae2e92289bb311d85e
MagicTINTIN <valentinservieres@gmail.com>
2023-11-26 17:22:43 +0200

    updt benchmark + add more sets

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,31 @@
+#!/bin/bash
+
+# PREPARATION
+rm benchmark.csv
+
+# SETTINGS
+
+nbRepetitions=4
+maxProcesses=40
+fill_character="#"
+
+# DISPLAY
+
+console_width=$(tput cols)
+sectionTerminator=""
+for ((i = 0; i < $console_width; i++)); do
+    sectionTerminator+=$fill_character
+done
+
+echo -ne "$sectionTerminator\nBenchmarking MPIFarm\n$sectionTerminator\n"
+echo "List of the image sets: "
+
+# CREATE SET LIST
+
 imageSets=(
     "P170B328_ServieresV_L3_smallest.json"
     "P170B328_ServieresV_L3_xxsmall.json"
+    "P170B328_ServieresV_L3_0001-0003.json"
     "P170B328_ServieresV_L3_0001-0012.json"
     "P170B328_ServieresV_L3_0001-0024.json"
     "P170B328_ServieresV_L3_0001-0064.json"
@@ -10,6 +35,57 @@ imageSets=(
     "P170B328_ServieresV_L3_0001-2500.json"
 )
 
+setSizes=(
+    1
+    19
+    3
+    12
+    24
+    64
+    128
+    512
+    1024
+    2500
+)
+
+setLabels=(
+    "1 images (9px)"
+    "19 images (9px)"
+    "3 images"
+    "12 images"
+    "24 images"
+    "64 images"
+    "128 images"
+    "512 images"
+    "1024 images"
+    "2500 images"
+)
+
+imageSets=(
+    "P170B328_ServieresV_L3_smallest.json"
+    "P170B328_ServieresV_L3_xxsmall.json"
+)
+
+setSizes=(
+    1
+    19
+)
+
+setLabels=(
+    "1 images (9px)"
+    "19 images (9px)"
+)
+
 for element in "${imageSets[@]}"; do
-    echo "$element"
+    echo "- $element"
+done
+
+# PREPARING CSV FILE
+
+# STARTING BENCHMARK
+
+for ((nbProcesses = 1; nbProcesses < $maxProcesses; nbProcesses++)); do
+    for element in "${imageSets[@]}"; do
+        echo "- $element ($nbProcesses)"
+    done
 done
